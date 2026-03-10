dbe6f902b980291637b0fc58fbb87c2efecac61f
Roman <gm.rim05@gmail.com>
2026-03-10 19:53:45 +0300

    add new methods in TaskController.java

--- src/main/java/ru/mikhailovri/ToDoList/controller/TaskController.java
+++ src/main/java/ru/mikhailovri/ToDoList/controller/TaskController.java
@@ -8,6 +8,9 @@ import ru.mikhailovri.ToDoList.entity.Error;
 import ru.mikhailovri.ToDoList.entity.Task;
 import ru.mikhailovri.ToDoList.service.TaskService;
 
+import java.util.List;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +18,7 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping("/tasks")
-    public ResponseEntity<?> createTask(@RequestBody Task task){
+    public ResponseEntity<?> createTask(@RequestBody Task task) {
         try {
             return ResponseEntity.ok(taskService.create(task));
         } catch (IllegalArgumentException e) {
@@ -26,4 +29,30 @@ public class TaskController {
         }
     }
 
+    @GetMapping("/tasks")
+    public List<?> getAll() {
+        return taskService.getAll();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<?> getTaskByID(@PathVariable("id") UUID taskUuid) {
+        try {
+            return ResponseEntity.ok(taskService.getByUuid(taskUuid));
+        } catch (IllegalArgumentException e) {
+            log.error("Ошибка при поиске задачи: {}", e.getMessage());
+            return ResponseEntity.badRequest().body(Error.builder()
+                    .message(e.getMessage()).build());
+        }
+    }
+
+    @PutMapping("/tasks/{id}")
+    public void updateTask (@PathVariable("id") UUID taskUuid,
+                                        @RequestBody Task newTask) {
+        taskService.update(taskUuid, newTask);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask (@PathVariable("id") UUID uuid) {
+        taskService.delete(uuid);
+    }
 }
